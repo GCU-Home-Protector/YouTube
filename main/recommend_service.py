07c6e22156db50a67d577fedaae3624fe60ba2ca
han16935 <han1693516@gmail.com>
2025-06-23 14:30:01 +0900

    fix : 컨테이너 내에서 csv 및 onnx 파일 접근 위해 변경

--- main/recommend_service.py
+++ main/recommend_service.py
@@ -1,37 +1,36 @@
+import os
 import pandas as pd
 import numpy as np
 
 def recommend(state, liked_songs):
-    sim_filename = f"{state}_similarity_matrix.csv"
+    sim_filename = os.path.join(os.path.dirname(__file__), "..", "main", f"{state}_similarity_matrix.csv")
+    # sim_filename = f"{state}_similarity_matrix.csv"
     sim_df = pd.read_csv(sim_filename, index_col=0)
+    # data = pd.read_csv('final_processed_data.csv')  # 여기로 이동
+    data = pd.read_csv(os.path.join(os.path.dirname(__file__), "..", "main", "final_processed_data.csv"))  # 여기로 이동
 
     if not liked_songs:
-        data = pd.read_csv('final_processed_data.csv')
         return filter_videos_corrected(data, tag=state, top_n=1)
 
-    # liked_songs에서 .mp3 확장자를 붙여 비교
     liked_songs_with_extension = [song + ".mp3" for song in liked_songs]
     valid_liked_songs = [song for song in liked_songs_with_extension if song in sim_df.index]
 
     if not valid_liked_songs:
-        return "좋아요 누른 노래들이 데이터베이스에 없습니다."
+        return filter_videos_corrected(data, tag=state, top_n=1)
 
-    # 모든 노래에 대해 좋아요 누른 노래들과의 평균 유사도 계산
     avg_similarities = {}
     for song in sim_df.columns:
         if song not in valid_liked_songs:
-            try:
-                similarities = [sim_df.loc[liked_song, song] for liked_song in valid_liked_songs]
-                avg_similarities[song] = np.mean(similarities)
-            except KeyError:
-                continue
+            similarities = [sim_df.loc[liked_song, song] for liked_song in valid_liked_songs]
+            avg_similarities[song] = np.mean(similarities)
 
     if not avg_similarities:
-        return "추천할 수 있는 노래를 찾을 수 없습니다."
+        return filter_videos_corrected(data, tag=state, top_n=1)
 
-    # 평균 유사도가 가장 높은 노래 반환
     recommended_song = max(avg_similarities, key=avg_similarities.get)
     return recommended_song
+
+
 
 def filter_videos_corrected(dataframe, tag, top_n=5):
     # 조회수 당 좋아요 비율
